40024dffe15e1190b51b5f1dd70da4424d0e0f1e
wbspeicher <wbspeicher1@gmail.com>
2018-04-04 13:26:50 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -4,16 +4,19 @@ using VolumeViewer;
 
 public class test : MonoBehaviour {
     public Texture2D noise;
+    public Material loading;
     
 	void Start () {
         MakeVolume(
             "Assets/VolumeViewerPro/examples/volumes/UTDesign DICOM/Atrial Switch/Atrial Switch CMR 3D SSFP/img0001-2721.13.dcm",
-            noise);
+            noise,
+            loading);
     }
 
-    public static GameObject MakeVolume(string path, Texture2D noise)
+    public static GameObject MakeVolume(string path, Texture2D noise = null, Material loading = null)
     {
         GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        g.GetComponent<MeshRenderer>().material = loading;
         VolumeComponent vc = g.AddComponent<VolumeComponent>();
         vc.rayOffset = noise;
         VolumeRenderer vr = Camera.main.GetComponent<VolumeRenderer>();
