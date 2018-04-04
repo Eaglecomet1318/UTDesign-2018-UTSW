648894502ef22ae1ab32d938007aa4ad4f343a2c
wbspeicher <wbspeicher1@gmail.com>
2018-04-03 20:24:06 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VolumeViewer;
+using System.IO;
 
 public class test : MonoBehaviour {
     
+    private static Texture2D _noise;
+    public static Texture2D noise { get {
+            if (_noise) return _noise;
+            _noise = new Texture2D(512, 512);
+            _noise.LoadImage(File.ReadAllBytes("Assets/VolumeViewerPro/textures/noise.png"));
+            return _noise;
+    } }
+
 	// Use this for initialization
 	void Start () {
         MakeVolume("Assets/VolumeViewerPro/examples/volumes/UTDesign DICOM/Atrial Switch/Atrial Switch CMR 3D SSFP/img0001-2721.13.dcm");
@@ -14,6 +23,7 @@ public class test : MonoBehaviour {
     {
         GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
         VolumeComponent vc = g.AddComponent<VolumeComponent>();
+        vc.rayOffset = noise;
         VolumeRenderer vr = Camera.main.GetComponent<VolumeRenderer>();
         if (!vr)
         {
