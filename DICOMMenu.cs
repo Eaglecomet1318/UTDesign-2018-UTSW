9a77dc446fcf3741c7bdfac4b965dbbe3759b5d1
Eaglecomet1318 <38547684+Eaglecomet1318@users.noreply.github.com>
2018-05-02 06:24:20 -0500

    Update DICOMMenu.cs

--- DICOMMenu.cs
+++ DICOMMenu.cs
@@ -1,32 +1,210 @@
 using UnityEngine;
 using VolumeViewer;
 using UnityEngine.UI;
+using VRTK;
+/* Semester: Spring 2018
+|------------------ public class DICOMMenu-------------------
+| This class displays a side panel of different modifications
+| that can be 
+*/
 
-public class DICOMMenu : MonoBehaviour {
+public class DICOMMenu : MonoBehaviour
+{
+    private static DICOMMenu _mInstance;
+    public static DICOMMenu mInstance {
+        get
+        {
+            if (!_mInstance)
+            {
+                _mInstance = Resources.FindObjectsOfTypeAll<DICOMMenu>()[0];
 
-    public static DICOMMenu mInstance { get; private set; }
-    public Slider cutValue;
-    public Button convertToSTL;
-    public VolumeComponent mainVolume;
+            }
+            return _mInstance;
+        }
+        private set
+        {
+            _mInstance = value;
+        }
+    }
+    public Slider cutValue;  
+    public Slider contrast;
+    public Slider height;
     
-    void Start () {
+      /* Sp18
+    | Transfer function buttons, each of which
+    | apllies 
+    */  
+    public Button convertToSTL;
+    public Button disableTF;
+    
+    /* Sp18
+    | Transfer function buttons, each of which
+    | apllies 
+    */
+    public Button redTF;
+    public Button darkRedTF;
+    public Button orangeTF;
+    public Button yellowTF;
+    public Button greenTF;
+    public Button blueTF;
+    public Button purpleTF;
+    public VolumeComponent mainVolume;
+
+    public void ToggleMenu()
+    {
+        Debug.Log("test");
+        mInstance.gameObject.SetActive(!mInstance.gameObject.activeSelf);
+    }
+
+    void Awake()
+    {
         mInstance = this;
         cutValue.onValueChanged.AddListener(delegate { CutValueChanged(); });
-        convertToSTL.onClick.AddListener(delegate { ConvertToSTL(); });
+        contrast.onValueChanged.AddListener(delegate { ContrastChanged(); });
+        convertToSTL.onClick.AddListener(delegate { Debug.Log("Convert to STL button pressed."); ConvertToSTL(); });
+        disableTF.onClick.AddListener(delegate { Debug.Log("Disable TF button pressed."); disableTransferFunction(); });
+        redTF.onClick.AddListener(delegate { Debug.Log("Red TF button pressed."); redTFChanged(); });
+        darkRedTF.onClick.AddListener(delegate { Debug.Log("Dark Red TF button pressed."); darkRedTFChanged(); });
+        orangeTF.onClick.AddListener(delegate { Debug.Log("Orange TF button pressed."); orangeTFChanged(); });
+        yellowTF.onClick.AddListener(delegate { Debug.Log("Yellow TF button pressed."); yellowTFChanged(); });
+        greenTF.onClick.AddListener(delegate { Debug.Log("Green TF button pressed."); greenTFChanged(); });
+        blueTF.onClick.AddListener(delegate { Debug.Log("Blue TF button pressed."); blueTFChanged(); });
+        purpleTF.onClick.AddListener(delegate { Debug.Log("Purple TF button pressed."); purpleTFChanged(); });
+        mainVolume.tfData = GetComponent<tfDataMode>().list[0]; // Default TF
     }
-	
+
+    public void Update()
+    {
+        GameObject left, right;
+        bool grabbing = false;
+        if((left = VRTK_DeviceFinder.GetControllerLeftHand()) && left.GetComponent<VRTK_InteractGrab>().GetGrabbedObject() != null)
+        {
+            grabbing = true;
+        }
+        if ((right = VRTK_DeviceFinder.GetControllerRightHand()) && right.GetComponent<VRTK_InteractGrab>().GetGrabbedObject() != null)
+        {
+            grabbing = true;
+        }
+        if(grabbing)
+        {
+            setAlpha(.5f);
+            GetComponent<VRTK_UICanvas>().enabled = false;
+        }
+        else
+        {
+            setAlpha(1f);
+            GetComponent<VRTK_UICanvas>().enabled = true;
+        }
+    }
+
+    public void setAlpha(float alpha)
+    {
+        CanvasRenderer[] children = GetComponentsInChildren<CanvasRenderer>();
+        Color newColor;
+        foreach (CanvasRenderer child in children)
+        {
+            newColor = child.GetColor();
+            newColor.a = alpha;
+            child.SetColor(newColor);
+        }
+    }
+
     public void CutValueChanged()
     {
         mainVolume.cutValueRangeMin = cutValue.normalizedValue;
+    }
+
+    public void ContrastChanged()
+    {
+        mainVolume.contrast = contrast.normalizedValue;
+    }
+
+    public void HeightChanged()
+    {
+        mainVolume.transform.position = new Vector3(0, height.normalizedValue + 1, 0);
     }
 
     public void ConvertToSTL()
     {
         if (!mainVolume.GetComponent<VolumeViewerMarcher>())
         {
-            Debug.LogWarning("Target Volume does not have a VolumeViewerMarcher component.");
+            Debug.LogError("Target Volume does not have a VolumeViewerMarcher component.");
             return;
         }
         mainVolume.GetComponent<VolumeViewerMarcher>().ConvertToMesh(mainVolume.cutValueRangeMin);
+    }
+
+    public void disableTransferFunction()
+    {
+        mainVolume.tfDataBlendMode = VolumeBlendMode.Disabled;
+    }
+
+    public void redTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[0];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
+    }
+
+    public void darkRedTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[1];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
+    }
+
+    public void orangeTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[2];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
+    }
+    public void yellowTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[3];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
+    }
+
+    public void greenTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[4];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
+    }
+
+    public void blueTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[5];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
+    }
+
+    public void purpleTFChanged()
+    {
+        mainVolume.tfData = GetComponent<tfDataMode>().list[6];
+
+        if (mainVolume.tfDataBlendMode == VolumeBlendMode.Disabled)
+        {
+            mainVolume.tfDataBlendMode = VolumeBlendMode.Multiply;
+        }
     }
 }
