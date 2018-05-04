059a20d0d894d67449bbee793575899e0e3678f7
Eaglecomet1318 <38547684+Eaglecomet1318@users.noreply.github.com>
2018-05-03 21:24:31 -0500

    Add files via upload

---
+++ tfDataMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+| public class tfDataMode serializes a list, each individual space of which has a potential transfer function.
+ */
+public class tfDataMode : MonoBehaviour
+{
+    [SerializeField]
+    public List<Texture2D> list;
+}
